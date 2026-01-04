3483c8161225605adde289df99d5803123898ee1
arebaza <arebaza@byupathway.edu>
2026-01-03 22:40:15 -0700

    exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string entradaUsuario = Console.ReadLine();
+
+        int porcentaje = int.Parse(entradaUsuario);
+
+        // Letter grade
+        string letra;
+
+        if (porcentaje >= 90)
+        {
+            letra = "A";
+        }
+        else if (porcentaje >= 80)
+        {
+            letra = "B";
+        }
+        else if (porcentaje >= 70)
+        {
+            letra = "C";
+        }
+        else if (porcentaje >= 60)
+        {
+            letra = "D";
+        }
+        else
+        {
+            letra = "F";
+        }
+
+        // Sign (+ or -)
+        string signo = "";
+        int ultimoDigito = porcentaje % 10;
+
+        if (ultimoDigito >= 7)
+        {
+            signo = "+";
+        }
+        else if (ultimoDigito < 3)
+        {
+            signo = "-";
+        }
+
+        // No A+
+        if (letra == "A" && signo == "+")
+        {
+            signo = "";
+        }
+
+        // No F+ or F-
+        if (letra == "F")
+        {
+            signo = "";
+        }
+
+        Console.WriteLine($"Your letter grade is {letra}{signo}.");
+
+        // Pass / Fail message
+        if (porcentaje >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying! You can do better next time.");
+        }
     }
 }
